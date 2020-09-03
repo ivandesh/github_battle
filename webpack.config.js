61012b9ba62b46e77387ca561cbfa7dcf86a193e
ivandesh <ivandesh203@gmail.com>
2020-09-03 15:36:11 +0300

    add client side rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpack = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
@@ -17,7 +18,10 @@ module.exports = {
   plugins: [
     new htmlWebpackPlugin({
       template: './app/index.html'
-    })
+    }),
+    new CopyWebpack([
+      { from: './_redirects' }
+    ])
   ],
   devServer: {
     historyApiFallback: true
